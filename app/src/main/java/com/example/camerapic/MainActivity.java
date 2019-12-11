df8319c479bbfab5fc057d629c6b8d12d241b8da
kcg-yyu <y_yu@kcg.ac.jp>
2019-12-11 23:20:50 +0900

    Commit for taking picture, loading from album and audio recording

---
+++ app/src/main/java/com/example/camerapic/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.camerapic;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends Activity{
+
+    private Button startbtn, stopbtn, playbtn, stopplay;
+    private MediaRecorder mRecorder;
+    private MediaPlayer mPlayer;
+    private static String mFileName = null;
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int RESULT_LOAD_IMAGE = 999;
+    private ImageView imageView;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startbtn = (Button)findViewById(R.id.btnRecord);
+        stopbtn = (Button)findViewById(R.id.btnStop);
+        playbtn = (Button)findViewById(R.id.btnPlay);
+        stopplay = (Button)findViewById(R.id.btnStopPlay);
+        stopbtn.setEnabled(false);
+        playbtn.setEnabled(false);
+        stopplay.setEnabled(false);
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/AudioRecording.3gp";
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+        Button albumButton = (Button) this.findViewById(R.id.button2);
+        if ( ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+        }
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+        albumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
+           ActivityCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.RECORD_AUDIO,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+        }
+
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                stopbtn.setEnabled(true);
+                startbtn.setEnabled(false);
+                playbtn.setEnabled(false);
+                stopplay.setEnabled(false);
+                mRecorder = new MediaRecorder();
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mRecorder.setOutputFile(mFileName);
+                try {
+                    mRecorder.prepare();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                mRecorder.start();
+            }
+        });
+
+        stopbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopbtn.setEnabled(false);
+                startbtn.setEnabled(true);
+                playbtn.setEnabled(true);
+                stopplay.setEnabled(true);
+                mRecorder.stop();
+                mRecorder.release();
+                mRecorder = null;
+            }
+        });
+
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopbtn.setEnabled(false);
+                startbtn.setEnabled(true);
+                playbtn.setEnabled(false);
+                stopplay.setEnabled(true);
+                mPlayer = new MediaPlayer();
+                try {
+                    mPlayer.setDataSource(mFileName);
+                    mPlayer.prepare();
+                    mPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        stopplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.release();
+                mPlayer = null;
+                stopbtn.setEnabled(false);
+                startbtn.setEnabled(true);
+                playbtn.setEnabled(true);
+                stopplay.setEnabled(false);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else {
+                //Error happen
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        } else if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
